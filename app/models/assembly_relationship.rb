b79826d017cd0997ced128285b1f253aed4e1ffe
Sathyan Mathai <sathyanmathai@Sathyans-MacBook-Pro.local>
2017-09-10 13:27:20 -0400

    Added custom validation for the assembly relationship

--- app/models/assembly_relationship.rb
+++ app/models/assembly_relationship.rb
@@ -1,4 +1,14 @@
 class AssemblyRelationship < ActiveRecord::Base
+  validate :different_assemblies
+
   belongs_to :assembly
   belongs_to :sub_assembly, class_name: "Assembly"
+
+  private
+
+  def different_assemblies
+    if assembly_id == sub_assembly_id
+      errors.add(:sub_assembly_id, "The sub-assembly cannot be the same as the assembly.")
+    end
+  end
 end
